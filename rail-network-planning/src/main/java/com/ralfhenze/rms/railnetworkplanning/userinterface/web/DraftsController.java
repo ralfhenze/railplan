c3baa18cd4de1edf40e8fd54bb32510b0a0a99b1
Ralf Henze <mail@ralfhenze.com>
2019-11-26 18:27:11 +0100

    Add dedicated page for each Draft

--- rail-network-planning/src/main/java/com/ralfhenze/rms/railnetworkplanning/userinterface/web/DraftsController.java
+++ rail-network-planning/src/main/java/com/ralfhenze/rms/railnetworkplanning/userinterface/web/DraftsController.java
@@ -2,8 +2,6 @@ package com.ralfhenze.rms.railnetworkplanning.userinterface.web;
 
 import com.ralfhenze.rms.railnetworkplanning.application.commands.AddRailNetworkDraftCommand;
 import com.ralfhenze.rms.railnetworkplanning.application.queries.Queries;
-import com.ralfhenze.rms.railnetworkplanning.domain.railnetwork.lifecycle.draft.RailNetworkDraft;
-import com.ralfhenze.rms.railnetworkplanning.domain.railnetwork.lifecycle.draft.RailNetworkDraftId;
 import com.ralfhenze.rms.railnetworkplanning.infrastructure.persistence.MongoDbQueries;
 import com.ralfhenze.rms.railnetworkplanning.infrastructure.persistence.RailNetworkDraftMongoDbRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +9,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DraftsController {
@@ -26,13 +25,22 @@ public class DraftsController {
         return "drafts";
     }
 
+    @GetMapping("/drafts/{draftId}")
+    public String draft(@PathVariable String draftId, Model model) {
+        final Queries queries = new MongoDbQueries(mongoTemplate);
+        model.addAttribute("draftIds", queries.getAllDraftIds());
+        model.addAttribute("currentDraftId", draftId);
+
+        return "drafts";
+    }
+
     @GetMapping("/drafts/new")
     public String createNewDraft() {
-        final String id =
+        final String draftId =
             new AddRailNetworkDraftCommand(new RailNetworkDraftMongoDbRepository(mongoTemplate))
             .addRailNetworkDraft()
             .get();
 
-        return "redirect:/drafts";
+        return "redirect:/drafts/" + draftId;
     }
 }
