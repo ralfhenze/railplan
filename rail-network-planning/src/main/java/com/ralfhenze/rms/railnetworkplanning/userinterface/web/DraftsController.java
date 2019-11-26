1a58d4249e3e69bd4707b637c332e5a752b2fff6
Ralf Henze <mail@ralfhenze.com>
2019-11-26 17:47:25 +0100

    Load listed Drafts from database

--- rail-network-planning/src/main/java/com/ralfhenze/rms/railnetworkplanning/userinterface/web/DraftsController.java
+++ rail-network-planning/src/main/java/com/ralfhenze/rms/railnetworkplanning/userinterface/web/DraftsController.java
@@ -1,17 +1,24 @@
 package com.ralfhenze.rms.railnetworkplanning.userinterface.web;
 
+import com.ralfhenze.rms.railnetworkplanning.application.queries.Queries;
+import com.ralfhenze.rms.railnetworkplanning.infrastructure.persistence.MongoDbQueries;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
-
 @Controller
 public class DraftsController {
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     @GetMapping("/drafts")
     public String drafts(Model model) {
-        model.addAttribute("drafts", Arrays.asList(1, 2, 3));
+        Queries queries = new MongoDbQueries(mongoTemplate);
+
+        model.addAttribute("draftIds", queries.getAllDraftIds());
 
         return "drafts";
     }
