919ff81df451836847b348595a504ede376fd911
Ralf Henze <mail@ralfhenze.com>
2019-11-28 15:39:34 +0100

    Add /drafts/{draftId}/stations GET endpoint

--- src/main/java/com/ralfhenze/railplan/userinterface/web/RestApiController.java
+++ src/main/java/com/ralfhenze/railplan/userinterface/web/RestApiController.java
@@ -2,6 +2,7 @@ package com.ralfhenze.railplan.userinterface.web;
 
 import com.ralfhenze.railplan.infrastructure.persistence.MongoDbQueries;
 import com.ralfhenze.railplan.infrastructure.persistence.dto.RailNetworkDraftDto;
+import com.ralfhenze.railplan.infrastructure.persistence.dto.TrainStationDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -16,12 +17,17 @@ public class RestApiController {
     private MongoTemplate mongoTemplate;
 
     @GetMapping("/drafts")
-    public List<RailNetworkDraftDto> drafts() {
+    public List<RailNetworkDraftDto> getDrafts() {
         return new MongoDbQueries(mongoTemplate).getAllDrafts();
     }
 
     @GetMapping("/drafts/{draftId}")
-    public RailNetworkDraftDto draft(@PathVariable String draftId) {
+    public RailNetworkDraftDto getDraft(@PathVariable String draftId) {
         return new MongoDbQueries(mongoTemplate).getDraftOfId(draftId);
+    }
+
+    @GetMapping("/drafts/{draftId}/stations")
+    public List<TrainStationDto> getStations(@PathVariable String draftId) {
+        return new MongoDbQueries(mongoTemplate).getDraftOfId(draftId).getStations();
     }
 }
