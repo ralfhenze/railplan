2106d184c2e112282496b3c6665dcff291ab2f1c
Ralf Henze <mail@ralfhenze.com>
2019-11-29 11:23:16 +0100

    Remove unneeded import statement

--- src/test/java/com/ralfhenze/railplan/userinterface/web/RestApiTest.java
+++ src/test/java/com/ralfhenze/railplan/userinterface/web/RestApiTest.java
@@ -2,7 +2,6 @@ package com.ralfhenze.railplan.userinterface.web;
 
 import com.ralfhenze.railplan.infrastructure.persistence.dto.RailNetworkDraftDto;
 import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
