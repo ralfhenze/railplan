0ad6a97020648229f70af70851f26e4d809e0ccd
Ralf Henze <mail@ralfhenze.com>
2019-12-13 21:10:25 +0100

    Add infos about Validity Period, Stations and Tracks to Network page

--- 04-userinterface/src/main/java/com/ralfhenze/railplan/userinterface/web/NetworksController.java
+++ 04-userinterface/src/main/java/com/ralfhenze/railplan/userinterface/web/NetworksController.java
@@ -1,11 +1,19 @@
 package com.ralfhenze.railplan.userinterface.web;
 
 import com.ralfhenze.railplan.application.queries.Queries;
+import com.ralfhenze.railplan.domain.railnetwork.lifecycle.release.ReleasedRailNetworkId;
 import com.ralfhenze.railplan.domain.railnetwork.lifecycle.release.ReleasedRailNetworkRepository;
+import com.ralfhenze.railplan.infrastructure.persistence.dto.ReleasedRailNetworkDto;
+import com.ralfhenze.railplan.infrastructure.persistence.dto.TrainStationDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class NetworksController {
@@ -30,5 +38,35 @@ public class NetworksController {
         model.addAttribute("networkIds", queries.getAllNetworkIds());
 
         return "networks";
+    }
+
+    /**
+     * Provides a Network page with all information (Validity Period, Stations and Tracks).
+     */
+    @GetMapping("/networks/{currentNetworkId}")
+    public String draft(@PathVariable String currentNetworkId, Model model) {
+        final var network = networkRepository
+            .getReleasedRailNetworkOfId(new ReleasedRailNetworkId(currentNetworkId));
+        final var networkDto = new ReleasedRailNetworkDto(network);
+
+        model.addAttribute("networkIds", queries.getAllNetworkIds());
+        model.addAttribute("currentNetwork", network);
+        model.addAttribute("tracks", getTracksWithStationNames(networkDto));
+
+        return "networks";
+    }
+
+    private List<List<String>> getTracksWithStationNames(
+        final ReleasedRailNetworkDto networkDto
+    ) {
+        final var stationNames = networkDto.getStations().stream()
+            .collect(Collectors.toMap(TrainStationDto::getId, TrainStationDto::getName));
+
+        return networkDto.getTracks().stream()
+            .map(trackDto -> List.of(
+                stationNames.get(trackDto.getFirstStationId()),
+                stationNames.get(trackDto.getSecondStationId())
+            ))
+            .collect(Collectors.toList());
     }
 }
--- 04-userinterface/src/test/java/com/ralfhenze/railplan/userinterface/web/NetworksControllerIT.java
+++ 04-userinterface/src/test/java/com/ralfhenze/railplan/userinterface/web/NetworksControllerIT.java
@@ -1,6 +1,12 @@
 package com.ralfhenze.railplan.userinterface.web;
 
 import com.ralfhenze.railplan.application.queries.Queries;
+import com.ralfhenze.railplan.domain.railnetwork.elements.GeoLocationInGermany;
+import com.ralfhenze.railplan.domain.railnetwork.elements.RailwayTrack;
+import com.ralfhenze.railplan.domain.railnetwork.elements.TrainStationName;
+import com.ralfhenze.railplan.domain.railnetwork.lifecycle.release.ReleasedRailNetwork;
+import com.ralfhenze.railplan.domain.railnetwork.lifecycle.release.ReleasedRailNetworkRepository;
+import org.eclipse.collections.api.factory.Lists;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.junit.Test;
@@ -15,7 +21,15 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import static com.ralfhenze.railplan.userinterface.web.TestData.berlinHbf;
+import static com.ralfhenze.railplan.userinterface.web.TestData.berlinHbfName;
+import static com.ralfhenze.railplan.userinterface.web.TestData.berlinHbfPos;
+import static com.ralfhenze.railplan.userinterface.web.TestData.defaultPeriod;
+import static com.ralfhenze.railplan.userinterface.web.TestData.hamburgHbf;
+import static com.ralfhenze.railplan.userinterface.web.TestData.hamburgHbfName;
+import static com.ralfhenze.railplan.userinterface.web.TestData.hamburgHbfPos;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
@@ -34,6 +48,9 @@ public class NetworksControllerIT {
     @MockBean
     private Queries queries;
 
+    @MockBean
+    private ReleasedRailNetworkRepository networkRepository;
+
     @Test
     public void userCanNavigateToExistingNetworks() throws Exception {
         // Given an existing Network ID "456"
@@ -51,7 +68,68 @@ public class NetworksControllerIT {
         assertThat(networkNavEntries.first().select("a").attr("href")).isEqualTo("/networks/456");
     }
 
+    @Test
+    public void userCanAccessAPageWithAllInfosAboutAReleasedRailNetwork() throws Exception {
+        // Given an existing Network
+        given(networkRepository.getReleasedRailNetworkOfId(any()))
+            .willReturn(getBerlinHamburgNetwork());
+
+        // When we call GET /networks/456
+        final var response = getGetResponse("/networks/456");
+
+        // Then we get start- and end-date of the Validity Period
+        final var document = Jsoup.parse(response.getContentAsString());
+        assertThat(response.getStatus()).isEqualTo(HTTP_OK);
+        assertThat(document.selectFirst(".valid-from").text())
+            .isEqualTo(defaultPeriod.getStartDate().toString());
+        assertThat(document.selectFirst(".valid-until").text())
+            .isEqualTo(defaultPeriod.getEndDate().toString());
+
+        // And we get a table with all Stations
+        final var stationRows = document.select("table#stations .station-row");
+        assertThat(stationRows).hasSize(2);
+        assertThatRowShowsNameAndLocation(stationRows.get(0), berlinHbfName, berlinHbfPos);
+        assertThatRowShowsNameAndLocation(stationRows.get(1), hamburgHbfName, hamburgHbfPos);
+
+        // And we get a table with all Tracks
+        final var trackRows = document.select("table#tracks .track-row");
+        assertThat(trackRows).hasSize(1);
+        assertThatRowShowsStationNames(trackRows.get(0), berlinHbfName, hamburgHbfName);
+    }
+
+    private void assertThatRowShowsNameAndLocation(
+        final Element row,
+        final TrainStationName stationName,
+        final GeoLocationInGermany location
+    ) {
+        assertThat(row.selectFirst(".stationName").text())
+            .isEqualTo(stationName.getName());
+        assertThat(row.selectFirst(".latitude").text())
+            .isEqualTo(location.getLatitudeAsString());
+        assertThat(row.selectFirst(".longitude").text())
+            .isEqualTo(location.getLongitudeAsString());
+    }
+
+    private void assertThatRowShowsStationNames(
+        final Element row,
+        final TrainStationName stationName1,
+        final TrainStationName stationName2
+    ) {
+        assertThat(row.selectFirst(".station-1").text())
+            .isEqualTo(stationName1.getName());
+        assertThat(row.selectFirst(".station-2").text())
+            .isEqualTo(stationName2.getName());
+    }
+
     private MockHttpServletResponse getGetResponse(final String url) throws Exception {
         return mockMvc.perform(get(url)).andReturn().getResponse();
+    }
+
+    private ReleasedRailNetwork getBerlinHamburgNetwork() {
+        return new ReleasedRailNetwork(
+            defaultPeriod,
+            Lists.immutable.of(berlinHbf, hamburgHbf),
+            Lists.immutable.of(new RailwayTrack(berlinHbf.getId(), hamburgHbf.getId()))
+        );
     }
 }
