a9733596ab020eeeb6641ba600bf244bb46f1ea4
Ralf Henze <mail@ralfhenze.com>
2019-12-27 20:52:59 +0100

    Port RailNetworkDraftId to PropertyValidation

--- 01-domain/src/main/java/com/ralfhenze/railplan/domain/railnetwork/lifecycle/draft/RailNetworkDraftId.java
+++ 01-domain/src/main/java/com/ralfhenze/railplan/domain/railnetwork/lifecycle/draft/RailNetworkDraftId.java
@@ -1,20 +1,30 @@
 package com.ralfhenze.railplan.domain.railnetwork.lifecycle.draft;
 
 import com.ralfhenze.railplan.domain.common.Id;
-import com.ralfhenze.railplan.domain.common.validation.Validation;
-import com.ralfhenze.railplan.domain.common.validation.ValidationException;
+import com.ralfhenze.railplan.domain.common.Validatable;
+import com.ralfhenze.railplan.domain.common.validation.PropertyValidation;
+import com.ralfhenze.railplan.domain.common.validation.ValidationError;
 import com.ralfhenze.railplan.domain.common.validation.constraints.IsNotBlank;
 
-public class RailNetworkDraftId implements Id {
+import java.util.List;
+
+public class RailNetworkDraftId implements Id, Validatable {
 
     private final String id;
 
-    public RailNetworkDraftId(final String id) throws ValidationException {
-        new Validation()
-            .ensureThat(id, new IsNotBlank(), "Rail Network Draft ID")
-            .throwExceptionIfInvalid();
-
+    public RailNetworkDraftId(final String id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean isValid() {
+        return getValidationErrors().isEmpty();
+    }
+
+    public List<ValidationError> getValidationErrors() {
+        return new PropertyValidation<>(id)
+            .ensureIt(new IsNotBlank())
+            .getValidationErrors();
     }
 
     @Override
