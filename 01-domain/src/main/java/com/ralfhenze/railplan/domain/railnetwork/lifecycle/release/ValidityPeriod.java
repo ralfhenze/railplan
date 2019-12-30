af9f892881f052b26ffb3580c2e23e12b964f0d8
Ralf Henze <mail@ralfhenze.com>
2019-12-30 16:37:46 +0100

    Port ValidityPeriod to PropertyValidation

--- 01-domain/src/main/java/com/ralfhenze/railplan/domain/railnetwork/lifecycle/release/ValidityPeriod.java
+++ 01-domain/src/main/java/com/ralfhenze/railplan/domain/railnetwork/lifecycle/release/ValidityPeriod.java
@@ -1,33 +1,36 @@
 package com.ralfhenze.railplan.domain.railnetwork.lifecycle.release;
 
+import com.ralfhenze.railplan.domain.common.Validatable;
 import com.ralfhenze.railplan.domain.common.ValueObject;
-import com.ralfhenze.railplan.domain.common.validation.Validation;
-import com.ralfhenze.railplan.domain.common.validation.ValidationException;
+import com.ralfhenze.railplan.domain.common.validation.PropertyValidation;
+import com.ralfhenze.railplan.domain.common.validation.ValidationError;
 import com.ralfhenze.railplan.domain.common.validation.constraints.IsBefore;
-import com.ralfhenze.railplan.domain.common.validation.constraints.IsNotNull;
 
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * [x] the TimePeriod's StartDate is before (<) EndDate
  */
-public class ValidityPeriod implements ValueObject {
+public class ValidityPeriod implements ValueObject, Validatable {
 
     private final LocalDate startDate;
     private final LocalDate endDate;
 
-    public ValidityPeriod(
-        final LocalDate startDate,
-        final LocalDate endDate
-    ) throws ValidationException {
-        new Validation()
-            .ensureThat(startDate, new IsNotNull(), "Start Date")
-            .ensureThat(endDate, new IsNotNull(), "End Date")
-            .ensureThat(startDate, new IsBefore(endDate), "Start Date")
-            .throwExceptionIfInvalid();
-
+    public ValidityPeriod(final LocalDate startDate, final LocalDate endDate) {
         this.startDate = startDate;
         this.endDate = endDate;
+    }
+
+    @Override
+    public boolean isValid() {
+        return getStartDateErrors().isEmpty();
+    }
+
+    public List<ValidationError> getStartDateErrors() {
+        return new PropertyValidation<>(startDate)
+            .ensureIt(new IsBefore(endDate))
+            .getValidationErrors();
     }
 
     public LocalDate getStartDate() {
