5ecc380caa5609c88a24a54e15b9e2174b16a3df
Ralf Henze <mail@ralfhenze.com>
2020-01-17 13:39:25 +0100

    Fix ReleasedRailNetwork unit tests

--- 01-domain/src/test/java/com/ralfhenze/railplan/domain/ReleasedRailNetworkUT.java
+++ 01-domain/src/test/java/com/ralfhenze/railplan/domain/ReleasedRailNetworkUT.java
@@ -1,17 +1,34 @@
 package com.ralfhenze.railplan.domain;
 
 import com.ralfhenze.railplan.domain.common.validation.ValidationException;
+import com.ralfhenze.railplan.domain.railnetwork.elements.GeoLocationInGermany;
 import com.ralfhenze.railplan.domain.railnetwork.elements.RailwayTrack;
+import com.ralfhenze.railplan.domain.railnetwork.elements.TrainStation;
+import com.ralfhenze.railplan.domain.railnetwork.elements.TrainStationId;
 import com.ralfhenze.railplan.domain.railnetwork.elements.TrainStationName;
 import com.ralfhenze.railplan.domain.railnetwork.lifecycle.release.ReleasedRailNetwork;
+import com.ralfhenze.railplan.domain.railnetwork.lifecycle.release.ValidityPeriod;
+import com.ralfhenze.railplan.domain.railnetwork.presets.PresetStation;
 import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.ImmutableList;
 import org.junit.Test;
 
-import static com.ralfhenze.railplan.domain.TestData.DEFAULT_PERIOD;
+import java.time.LocalDate;
+
+import static com.ralfhenze.railplan.domain.TestData.BERLIN_OST_LAT;
+import static com.ralfhenze.railplan.domain.TestData.BERLIN_OST_LNG;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class ReleasedRailNetworkUT {
+
+    private final static TrainStation BERLIN_HBF = getStation(1, PresetStation.BERLIN_HBF);
+    private final static TrainStation HAMBURG_HBF = getStation(2, PresetStation.HAMBURG_HBF);
+    private final static TrainStation FRANKFURT_HBF = getStation(3, PresetStation.FRANKFURT_HBF);
+    private final static TrainStation STUTTGART_HBF = getStation(4, PresetStation.STUTTGART_HBF);
+    private static final ValidityPeriod DEFAULT_PERIOD = new ValidityPeriod(
+        LocalDate.of(2019, 11, 14),
+        LocalDate.of(2019, 11, 20)
+    );
 
     @Test
     public void ensuresAtLeastTwoStations() {
@@ -76,12 +93,18 @@ public class ReleasedRailNetworkUT {
 
     @Test
     public void ensuresMinimumStationDistance() {
+        final var berlinOst = new TrainStation(
+            new TrainStationId(5),
+            new TrainStationName("Berlin Ostbahnhof"),
+            new GeoLocationInGermany(BERLIN_OST_LAT, BERLIN_OST_LNG)
+        );
+
         assertThatExceptionOfType(ValidationException.class).isThrownBy(() -> {
             new ReleasedRailNetwork(
                 DEFAULT_PERIOD,
-                listOf(BERLIN_HBF, BERLIN_OST, HAMBURG_HBF),
+                listOf(BERLIN_HBF, berlinOst, HAMBURG_HBF),
                 listOf(
-                    new RailwayTrack(BERLIN_HBF.getId(), BERLIN_OST.getId()),
+                    new RailwayTrack(BERLIN_HBF.getId(), berlinOst.getId()),
                     new RailwayTrack(BERLIN_HBF.getId(), HAMBURG_HBF.getId())
                 )
             );
@@ -108,5 +131,16 @@ public class ReleasedRailNetworkUT {
 
     private <T> ImmutableList<T> emptyList() {
         return Lists.immutable.empty();
+    }
+
+    private static TrainStation getStation(
+        final int id,
+        final PresetStation presetStation
+    ) {
+        return new TrainStation(
+            new TrainStationId(id),
+            new TrainStationName(presetStation.getName()),
+            new GeoLocationInGermany(presetStation.getLatitude(), presetStation.getLongitude())
+        );
     }
 }
--- 01-domain/src/test/java/com/ralfhenze/railplan/domain/TestData.java
+++ 01-domain/src/test/java/com/ralfhenze/railplan/domain/TestData.java
@@ -1,16 +1,6 @@
 package com.ralfhenze.railplan.domain;
 
-import com.ralfhenze.railplan.domain.railnetwork.lifecycle.release.ValidityPeriod;
-
-import java.time.LocalDate;
-
 public class TestData {
-
     public static final double BERLIN_OST_LAT = 52.510784;
     public static final double BERLIN_OST_LNG = 13.434832;
-
-    public static final ValidityPeriod DEFAULT_PERIOD = new ValidityPeriod(
-        LocalDate.of(2019, 11, 14),
-        LocalDate.of(2019, 11, 20)
-    );
 }
