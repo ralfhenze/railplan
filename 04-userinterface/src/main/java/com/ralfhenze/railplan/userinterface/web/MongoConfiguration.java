b289583b3c6197f4f75bc95bcf89627ca708af16
Ralf Henze <mail@ralfhenze.com>
2020-01-23 19:22:01 +0100

    Make MongoDB credentials configurable

--- 04-userinterface/src/main/java/com/ralfhenze/railplan/userinterface/web/MongoConfiguration.java
+++ 04-userinterface/src/main/java/com/ralfhenze/railplan/userinterface/web/MongoConfiguration.java
@@ -2,6 +2,7 @@ package com.ralfhenze.railplan.userinterface.web;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
@@ -9,21 +10,41 @@ import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 
 /**
- * This is needed to prevent the default "_class" field of Spring Data's Mongo implementation
+ * This is needed to prevent the default "_class" field of Spring Data's MongoDB implementation
  *
  * https://docs.spring.io/spring-data/data-mongodb/docs/current/reference/html/#mongo-template.type-mapping
  */
 @Configuration
 public class MongoConfiguration extends AbstractMongoClientConfiguration {
 
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private String port;
+
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+
+    @Value("${spring.data.mongodb.password}")
+    private String password;
+
     @Override
     protected String getDatabaseName() {
-        return "rms";
+        return databaseName;
     }
 
     @Override
     public MongoClient mongoClient() {
-        return MongoClients.create();
+        var user = "";
+        if (!username.isBlank()) {
+            user = username + ":" + password + "@";
+        }
+
+        return MongoClients.create("mongodb://" + user + host + ":" + port);
     }
 
     @Bean
